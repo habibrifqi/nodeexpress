440e4292e733395f77467364dc5407bfb043adf9
habibrifqi <habib.rifqi.16@gmail.com>
2023-04-13 11:19:29 +0700

    membuat crud sederhana

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
 const nodemon = require("nodemon");
-const app = express();
-const userRouter = require("./users");
 
+const userRouter = require("./users");
+const app = express();
 const port = 3000;
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) 
+
 
 app.get("/", (req, res) => {
     // console.log("sad")
--- users.js
+++ users.js
@@ -2,25 +2,50 @@ const express = require("express");
 const Router = express.Router();
 //users
 let users = [
-    {id:1,name:"habib",pass:111},
-    {id:2,name:"rifqi",pass:222}
-]
+  { id: 1, name: "habib", pass: 111 },
+  { id: 2, name: "rifqi", pass: 222 },
+];
 
 Router.route("/users")
   .get((req, res) => {
-    res.json(users);
+    if (users.length > 0) {
+      res.json({
+        status: true,
+        data: users,
+        method: req.method,
+        url: req.url,
+      });
+    } else {
+      res.json({
+        status: false,
+        message: "data ksoong"
+      });
+    }
   })
   .post((req, res) => {
-    res.send("Post User");
+    users.push(req.body);
+    res.send(users);
   });
 
 Router.put("/users/:id", (req, res) => {
-    const id = req.params
-    res.send(id);
+  const id = req.params.id;
+  users.filter((user) => {
+    if (user.id == id) {
+      user.id = id;
+      user.name = req.body.name;
+      user.pass = req.body.pass;
+
+      return user;
+    }
+  });
+  res.json(users);
 });
 
 Router.delete("/users/:userId", (req, res) => {
-    res.send(req.params);
+  let id = req.params.userId;
+  users = users.filter((user) => user.id != id);
+
+  res.send(users);
 });
 
 module.exports = Router;
