7f577ec661dc2bfc34a2499e8c57ac9cbd35a276
habibrifqi <habib.rifqi.16@gmail.com>
2023-04-14 14:35:03 +0700

    membuat store untuk menambah data

--- controllers/user.js
+++ controllers/user.js
@@ -1,52 +1,55 @@
 let users = [
-    { id: 1, name: "habib", pass: 111 },
-    { id: 2, name: "rifqi", pass: 222 },
-  ];
+  { id: 1, name: "habib", pass: 111 },
+  { id: 2, name: "rifqi", pass: 222 },
+];
 
 module.exports = {
-    index : (req, res) => {
-        res.render('pages/user/index', {users :users})
-        // if (users.length > 0) {
-        //   res.json({
-        //     status: true,
-        //     data: users,
-        //     method: req.method,
-        //     url: req.url,
-        //   });
-        // } else {
-        //   res.json({
-        //     status: false,
-        //     message: "data ksoong"
-        //   });
-        // }
-      },
-    store : (req, res) => {
-        users.push(req.body);
-        res.send({
-            status :true,
-            message: "data user berhasil di simpan",
-            data :users,
-            method: req.method,
-            url: req.url,
-        });
-    },
-    update : (req, res) => {
-        const id = req.params.id;
-        users.filter((user) => {
-          if (user.id == id) {
-            user.id = id;
-            user.name = req.body.name;
-            user.pass = req.body.pass;
-      
-            return user;
-          }
-        });
-        res.json(users);
-      },
-      delete : (req, res) => {
-        let id = req.params.userId;
-        users = users.filter((user) => user.id != id);
-      
-        res.send(users);
+  index: (req, res) => {
+    res.render("pages/user/index", { users: users });
+    // if (users.length > 0) {
+    //   res.json({
+    //     status: true,
+    //     data: users,
+    //     method: req.method,
+    //     url: req.url,
+    //   });
+    // } else {
+    //   res.json({
+    //     status: false,
+    //     message: "data ksoong"
+    //   });
+    // }
+  },
+  create: (req, res) => {
+    res.render("pages/user/create");
+  },
+  store: (req, res) => {
+    users.push(req.body);
+    res.send({
+      status: true,
+      message: "data user berhasil di simpan",
+      data: users,
+      method: req.method,
+      url: req.url,
+    });
+  },
+  update: (req, res) => {
+    const id = req.params.id;
+    users.filter((user) => {
+      if (user.id == id) {
+        user.id = id;
+        user.name = req.body.name;
+        user.pass = req.body.pass;
+
+        return user;
       }
-}
+    });
+    res.json(users);
+  },
+  delete: (req, res) => {
+    let id = req.params.userId;
+    users = users.filter((user) => user.id != id);
+
+    res.send(users);
+  },
+};
--- router/users.js
+++ router/users.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const Router = express.Router();
 
-const usercontroller = require('../controllers/user');
+const usercontroller = require("../controllers/user");
 //users
 
+Router.route("/users").get(usercontroller.index).post(usercontroller.store);
 
-Router.route("/users")
-  .get(usercontroller.index)
-  .post(usercontroller.store);
+Router.get('/users/create', usercontroller.create)
 
-Router.put("/users/:id",usercontroller.update);
+Router.put("/users/:id", usercontroller.update);
 
-Router.delete("/users/:userId",usercontroller.delete);
+Router.delete("/users/:userId", usercontroller.delete);
 
 module.exports = Router;
